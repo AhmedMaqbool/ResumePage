4a41b2e860a3cdb6a5807823272493747a2bc759
Ahmed Maqbool <39122332+AhmedMaqbool@users.noreply.github.com>
2020-08-06 11:19:38 +0300

    Updates

---
+++ precache-manifest.a39f67c0fa4529be122c565c418de62f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8aed64f8d18eab891f80abe993a5a1",
+    "url": "/ResumePage/index.html"
+  },
+  {
+    "revision": "1b1ca722be621e9c8784",
+    "url": "/ResumePage/static/css/main.8d8027eb.chunk.css"
+  },
+  {
+    "revision": "0f61e8b4f7bcabd9308d",
+    "url": "/ResumePage/static/js/2.2f60f854.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ResumePage/static/js/2.2f60f854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b1ca722be621e9c8784",
+    "url": "/ResumePage/static/js/main.46ef3c68.chunk.js"
+  },
+  {
+    "revision": "dc2fb3ef4a97669ac195",
+    "url": "/ResumePage/static/js/runtime-main.48766858.js"
+  },
+  {
+    "revision": "fd9eebcfbb0d9425bc35a98975d63d8c",
+    "url": "/ResumePage/static/media/profile.fd9eebcf.jpg"
+  }
+]);
